57184c21118d82b3921cc0b56862dc2518f9c70b
dimstar <kylermagee@gmail.com>
2019-04-04 21:52:51 -0700

    updates to logs and readme

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 Just messing around a little. Really thought giphy image delivery was interesting. What would that take?
 
-Images borrowd from this amazing post from [wikimediafoundation.org](https://wikimediafoundation.org/2018/12/20/presenting-the-2018-winners-from-the-worlds-largest-photo-contest/)
+Images borrowed from this amazing post from [wikimediafoundation.org](https://wikimediafoundation.org/2018/12/20/presenting-the-2018-winners-from-the-worlds-largest-photo-contest/)
--- index.js
+++ index.js
@@ -6,15 +6,15 @@ const port = 1985;
 app.get('/assets/:asset', (req, res) => {
   let {asset} = req.params;
   console.log(asset);
-  // res.send(`you want some ${asset}? /assets/${asset}`);
 
   fs.readFile(`./assets/${asset}`, function(err, data) {
+    if(err) throw err;
     res.writeHead(200, {
       "Content-Type": "image/jpg",
     });
     res.write(data);
     res.end();
   });
-})
+});
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, () => console.log(`listening on port ${port}!`))
